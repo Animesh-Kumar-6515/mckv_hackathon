3f2ded0e9282a10382f314ee7567b74840d0a71b
Sagar Shaw <sagarshaw800@gmail.com>
2022-12-23 17:22:56 +0530

    hotel sectio completed with animation

--- src/Components/Hotel/Hotel.jsx
+++ src/Components/Hotel/Hotel.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Box, Paper, Typography } from "@mui/material";
 import { motion } from "framer-motion";
-import styled from "@emotion/styled";
-import { grey } from "@mui/material/colors";
-import hotelImage1 from "../../Images/hotelimg1.jpg";
+import { styled } from "@mui/material/styles";
+import hotel1 from "../../Images/hotel1.jpg";
 
 const Hotel = () => {
   const StyledButton = styled(motion.button)({
@@ -13,58 +12,63 @@ const Hotel = () => {
     fontWeight: "bolder",
     paddingBottom: "10px",
   });
-  const StyledPaper = styled(Box)({
-    // backgroundImage: `url(${hotelImage1})`,
-    backgroundColor:"black",
+  const StyledBox = styled(motion.div)(({ theme }) => ({
+    backgroundImage: `url(${hotel1})`,
     borderRadius: "8px",
     height: "400px",
-    maxWidth: {xs:"98%",sm:"48%",md:"24%"},
     display: "flex",
     flexDirection: "column",
     justifyContent: "flex-end",
     alignItems: "center",
+    textAlign: "center",
     color: "white",
-    margin:"20px 0"
-  });
+    margin: "20px 0",
+    [theme.breakpoints.up("xs")]: {
+      width: "98%",
+    },
+    [theme.breakpoints.up("sm")]: {
+      width: "48%",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "24%",
+    },
+  }));
 
   const HotelName = styled(Typography)({
     fontWeight: "bold",
     fontSize: "20px",
-    paddingBottom: "5px",
+    padding: "15px 10px",
   });
+
   return (
-    <Box sx={{
-      display:"flex",
-      flexWrap:"wrap",
-      justifyContent:"space-around",
-      alignItems:"center",
-      padding:"5%"
-      
-    }}>
-      <StyledPaper>
-        <HotelName>Hyatt Centric Candolim Goa</HotelName>
-        <StyledButton whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }}>
-          View Hotel →
-        </StyledButton>
-      </StyledPaper>
-      <StyledPaper>
-        <HotelName>Hyatt Centric Candolim Goa</HotelName>
-        <StyledButton whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }}>
-          View Hotel →
-        </StyledButton>
-      </StyledPaper>
-      <StyledPaper>
-        <HotelName>Hyatt Centric Candolim Goa</HotelName>
-        <StyledButton whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }}>
-          View Hotel →
-        </StyledButton>
-      </StyledPaper>
-      <StyledPaper>
-        <HotelName>Hyatt Centric Candolim Goa</HotelName>
-        <StyledButton whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }}>
-          View Hotel →
-        </StyledButton>
-      </StyledPaper>
+    <Box sx={{display:'flex', flexFlow: 'column', alignItems:'center', paddingTop:'40px'}}>
+      <Box>
+        <Typography variant='h4'>Discover Something New</Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexFlow: "wrap",
+          justifyContent: "space-around",
+          alignItems: "center",
+          width: '100%',
+          padding: "30px 5%",
+        }}
+      >
+        {[
+          "Hyatt Centric Goa",
+          "Grand Hyatt Goa",
+          "Hyatt Regency Amritsar",
+          "Hyatt Regency Chandigarh",
+        ].map((hotel) => (
+          <StyledBox whileHover={{ translateY: -15 }} transition={{duration: 0.4}}>
+            <HotelName>{hotel}</HotelName>
+            <StyledButton whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }}>
+              View Hotel →
+            </StyledButton>
+          </StyledBox>
+        ))}
+      </Box>
     </Box>
   );
 };
